ea4222c3e0fa5f905536f7ea734fec76004f09db
rocco de luca <roccodeluca865@gmail.com>
2020-06-04 17:09:37 +0200

    added costructor

--- index.php
+++ index.php
@@ -7,20 +7,19 @@
      public $title;
      public $year;
      public $genre;
+
+     // costructor
+     public function  __construct($title, $year, $genre){
+        $this->title = $title;
+        $this->year = $year;
+        $this->genre = $genre;
+     }
  }
 
- $sonic = new Movie();
- $sonic->title = 'sonic';
- $sonic->year = 2020;
- $sonic->genre = 'azione fantascienza commedia avventura';
-
- $avatar = new Movie();
- $avatar->title = 'avatar';
- $avatar->year = 2009;
- $avatar->genre = 'azione fantascienza avventura';
+ $sonic = new Movie('sonic',2020,'azione fantascienza commedia avventura');
  
- //var_dump('test');
-//die();
+ $avatar = new Movie('avatar',2009,'azione fantascienza avventura');
+ 
  // display
 echo "title: $sonic->title<br>";
 echo "year: $sonic->year<br>";
